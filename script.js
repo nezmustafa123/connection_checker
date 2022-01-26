a654c5b7beba9624c05260364b811200c2a6c5a1
Nez Mustafa <nezmustafa123@gmail.com>
2022-01-26 23:10:54 +0000

    check the connection using http status code

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-// window.addEventListener("load", (event) => {
-//   //add event listener to window get tag then change the text depenging on naviagotr api
-//   const statusDisplay = document.getElementById("status"); //get the paragram
-//   statusDisplay.textContent = navigator.onLine ? "Online" : "Offline";
-// });
-
-// window.addEventListener("offline", (event) => {
-//   //add event listener to window get tag then change the text depenging on naviagotr api
-//   const statusDisplay = document.getElementById("status"); //get the paragram
-//   statusDisplay.textContent = "Offline";
-// });
-
-// window.addEventListener("online", (event) => {
-//   //add event listener to window get tag then change the text depenging on naviagotr api
-//   const statusDisplay = document.getElementById("status"); //get the paragram
-//   statusDisplay.textContent = "Online";
-// });
-
 // variables
 
-const image = document.getElementById('image');
-const statusDisplay = document.getElementById('status');
-const bgColor = document.getElementById('main');
+const image = document.getElementById("image");
+const statusDisplay = document.getElementById("status");
+const bgColor = document.getElementById("main");
 
+function setColor() {
+  bgColor.classList.add("online");
+}
 //fetch something external from server
 
-async function connectionStatus () {
-try {
-  const fetchResult = await fetch('https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png?time=' + (new Date()).getTime());
-  image.src="";
-}
+async function connectionStatus() {
+  try {
+    const fetchResult = await fetch(
+      "https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png?time=" +
+        new Date().getTime()
+    );
+    image.src = "./images/online.png";
+    setColor();
+    return fetchResult.status >= 200 && fetchResult.satus < 300;
+    //http status code between 200 and 300
+    // check to see intenet connetcion is active
+  } catch (error) {
+    console.log(error);
+  }
 }
